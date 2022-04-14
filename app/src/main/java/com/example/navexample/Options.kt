0334d8f82ba7e1d3ea8b46fc8273ba30afdf5cf0
Yury Prudnikov <y.prudnikov@growapp.biz>
2022-04-14 12:46:20 +0300

    add base fragment navigation

---
+++ app/src/main/java/com/example/navexample/Options.kt
@@ -0,0 +1,16 @@
+package com.example.navexample
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Options(
+    val boxCount: Int,
+    val isTimerEnabled: Boolean
+) : Parcelable {
+
+    companion object {
+        val DEFAULT = Options(boxCount = 3, isTimerEnabled = false)
+    }
+
+}
